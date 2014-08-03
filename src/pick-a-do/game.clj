9593eddb2cff42957324cecdbf50862ec89357b8
Aleksandar Terentic <aterentic@gmail.com>
2014-08-03 23:05:46 +0200

    Unfinished work on generator and game.

--- src/pick-a-do/game.clj
+++ src/pick-a-do/game.clj
@@ -1,8 +1,28 @@
 (ns pickado.game)
 
+(defn- calculate-score [player field weight]
+  (+ (or (field player) 0) weight -3))
+
+(calculate-score {:12 1} :15 2)
+
+(defn play- [player hit]
+  (let [field (key hit)
+        weight (val hit)]
+    (assoc player field (+ (or (field player) 0) weight))))
+
+(defn score [player hit]
+  (let [field (key hit)
+        weight (val hit)]
+    (assoc player field (+ (or (field player) 0) weight))))
+
+
 (defn throw-dart [player hit]
   (let [entry (first hit)
         field (key entry)
         value (val entry)]
     (assoc player field (+ value (if (contains? player field) (field player) 0)))))
 
+;; Pickado
+;; -------
+;; Players play game. In one player round player throws three darts.
+;; In one dart throw, dart hits table, adds score to player, and score other players.
