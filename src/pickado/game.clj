7d7ccd25ceab4c3e6cb47fab2bc13f30ae6ccb44
Aleksandar Terentic <aleksandar.terentic@seavus.com>
2014-08-15 08:20:46 +0200

    Refactoring game and generator.

--- src/pickado/game.clj
+++ src/pickado/game.clj
@@ -3,15 +3,10 @@
         [pickado.score :as score]
         [pickado.rules :as rules]))
 
-(defn throw-dart [{:keys [fields field-area center-area center-to-field-ratio]}]
-  (let [center? (< (rand) center-to-field-ratio)
-        field (if center? :center (keyword (str (generator/field fields (rand)))))
-        multiplier (generator/multiplier (if center? center-area field-area) (rand))]
-    {:field field :multiplier multiplier}))
-
 (defn player-turn [player hit]
-  (let [{:keys [field multiplier]} hit]
-    (assoc player field (score/player-score player hit))))
+    (assoc player
+      (:field hit)
+      (score/player-score player hit)))
 
 (defn score-value [score-amount]
   (let [{:keys [field multiplier]} score-amount]
@@ -30,24 +25,11 @@
         (player-turn (first players) hit)
         (map #(score-player (score/other-score (first players) hit) %) (rest players)))))
 
-(defn new-player [number]
-  (reduce #(assoc %1 %2 0)
-          {:center 0 :score 0 :number number}
-          (map (comp keyword str) (range 1 21))))
-
-(defn new-game [players]
-  {:field-area [0 0.80 0.90 1]
-   :center-area [0 0.75 1]
-   :center-to-field-ratio 1/61
-   :fields 20
-   :players (vec (map new-player (range 1 players)))
-   :round 0})
-
 (defn next-player [players]
   (conj (vec (rest players)) (first players)))
 
 (defn move [game]
-  (let [hit (throw-dart game)]
+  (let [hit (generator/throw-dart game rand)]
     (update-in game [:players] #(turn % hit))))
 
 (defn three-moves [game]
@@ -58,3 +40,16 @@
 
 (defn match [game rounds]
   (nth (iterate round game) rounds))
+
+(defn new-player [number]
+  (reduce #(assoc %1 %2 0)
+          {:center 0 :score 0 :number number}
+          (map (comp keyword str) (range 1 21))))
+
+(defn new-game [players]
+  {:field-area [0 0.80 0.90 1]
+   :center-area [0 0.75 1]
+   :center-to-field-ratio 1/61
+   :fields 20
+   :players (vec (map new-player (range 1 (inc players))))
+   :round 0})
--- src/pickado/generator.clj
+++ src/pickado/generator.clj
@@ -8,3 +8,13 @@
 (defn multiplier [ranges random]
   "Calculates multiplier from given ranges [0...1] and random number [0,1)."
   (count (filter #(<= % random) ranges)))
+
+(defn- center? [center-to-field-ratio randomizer]
+  (< (randomizer) center-to-field-ratio))
+
+(defn throw-dart [{:keys [fields field-area center-area center-to-field-ratio]} randomizer]
+  (if (center? center-to-field-ratio randomizer)
+    {:field :center
+     :multiplier (multiplier center-area (randomizer))}
+    {:field (keyword (str (field fields (randomizer))))
+     :multiplier (multiplier field-area (randomizer))}))
